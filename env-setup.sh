df5746da447a930bc3560897762c381b2b45d6ca
msalim <msalim@anl.gov>
2019-04-10 22:53:55 -0500

    bugfix

--- env-setup.sh
+++ env-setup.sh
@@ -4,4 +4,4 @@ module load cray-python/3.6.1.1
 mkdir -p $BUILD_ROOT
 export PYTHONUSERBASE=$BUILD_ROOT/.pip/theta/cray-python
 pip install --user virtualenv
-virtualenv  --system-site-packages --no-wheel $BUILD_ROOT/env
+$PYTHONUSERBASE/bin/virtualenv  --system-site-packages --no-wheel $BUILD_ROOT/env
